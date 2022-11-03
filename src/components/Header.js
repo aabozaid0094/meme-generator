31c9bd66f6a5430014adc0d8b9caa489bbc1d331
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-11-03 21:26:00 +0200

    Add custom header component and its structure

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = (props) => {
+    return (
+        <header>
+            <div className="logo-title">
+                <img className="logo" src={props.logo_src} />
+                <h1 className="header-title">{props.title}</h1>
+            </div>
+            <div className="subtitle">
+                <h2 className="header-subtitle">{props.subtitle}</h2>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
