c8b2e982fef0ce5ec1ddd111171abefe42fcff72
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-11-03 22:09:21 +0200

    Add app logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Logo from './troll-face.png';
 import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header title="Meme Generator" logo_src="" subtitle="React Course - Project 3" />
+      <Header title="Meme Generator" logo_src={Logo} subtitle="React Course - Project 3" />
     </div>
   );
 }
