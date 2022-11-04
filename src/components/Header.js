7e54df1b94dcca2e21a53a28d1e5da339da239d6
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-11-04 10:12:42 +0200

    Add logo alt

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = (props) => {
     return (
         <header className="app-header">
             <div className="logo-title">
-                <img className="logo" src={props.logo_src} />
+                <img className="logo" src={props.logo_src} alt="app-logo" />
                 <h1 className="header-title">{props.title}</h1>
             </div>
             <div className="subtitle">
