04c57ae4c656324e8bee29399401e375a954fa79
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-11-08 22:10:13 +0200

    Adding top/bottom text to the meme result

--- src/components/MemeResult.js
+++ src/components/MemeResult.js
@@ -4,9 +4,11 @@ const MemeResult = (props) => {
             <img
                 className="meme-result-image"
                 id="meme_result_image"
-                src={props.image.url}
+                src={props.meme.url}
                 alt="meme-result"
             />
+            <h2 className="top-text" id="top_text">{props.meme.top_text}</h2>
+            <h2 className="bottom-text" id="bottom_text">{props.meme.bottom_text}</h2>
         </div>
     );
 };
