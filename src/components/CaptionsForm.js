cba418b2e2fb79098ffc741be08bbe5e90c1891a
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-11-08 22:08:55 +0200

    Add name, value and onchange handler to inputs

--- src/components/CaptionsForm.js
+++ src/components/CaptionsForm.js
@@ -6,11 +6,17 @@ const CaptionsForm = (props) => {
                     className="form-input"
                     type="text"
                     placeholder="Top Text"
+                    name="top_text"
+                    value={props.meme.top_text}
+                    onChange={props.handle_change}
                 />
                 <input
                     className="form-input"
                     type="text"
                     placeholder="Buttom Text"
+                    name="bottom_text"
+                    value={props.meme.bottom_text}
+                    onChange={props.handle_change}
                 />
             </div>
             <div className="form-group">
